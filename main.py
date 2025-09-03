355f94d8430c0dfa9c8520dc8f710f01fa093658
Sifiso Mawila <Sifiso@nebula.co.za>
2025-09-04 01:30:38 +0200

    first-commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.preprocessing.text import Tokenizer
+import random
+import re
