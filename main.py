690ca39d87f44f34172ebfa49748faf12feebd5b
Sifiso Mawila <Sifiso@nebula.co.za>
2025-09-04 19:07:11 +0200

    Tune model for better performance No.3, increase epochs to achiever high accuracy (~100%) and low loss (~0.18-0.24)

--- main.py
+++ main.py
@@ -34,13 +34,13 @@ X_train = tokenizer.texts_to_matrix(training_sentences, mode="binary")  # multi-
 # Compile with adam optimizer and sparse categorical crossentropy loss
 # Train on X_train and y_train for 400 epochs
 model = Sequential([
-    Dense(18, input_shape=(X_train.shape[1],), activation="relu"),
+    Dense(26, input_shape=(X_train.shape[1],), activation="relu"),
     Dropout(0.2),
-    Dense(16, activation="relu"),
+    Dense(26, activation="relu"),
     Dense(len(labels), activation="softmax")
 ])
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-model.fit(X_train, y_train, epochs=500, verbose=1)
+model.fit(X_train, y_train, epochs=200, verbose=1)
 
 
 if __name__ == "__main__":
